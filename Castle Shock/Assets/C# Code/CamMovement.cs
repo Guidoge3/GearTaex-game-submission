bcfcf45a622a7ae66c1ede622b9a52ae0561c04b
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2021-11-10 22:13:32 +0100

    added newest files + movement stuff

---
+++ Castle Shock/Assets/C# Code/CamMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour
+{
+    private struct PointInSpace
+    {
+        public Vector3 Position;
+        public float Time;
+    }
+
+    public Transform target;
+    private Vector3 offset;
+    private float delay = 0.1f;
+    private float speed = 5;
+    private Queue<PointInSpace> pointsInSpace = new Queue<PointInSpace>();
+
+    void LateUpdate()
+    {
+
+        pointsInSpace.Enqueue(new PointInSpace() { Position = target.position, Time = Time.time });
+
+
+        while (pointsInSpace.Count > 0 && pointsInSpace.Peek().Time <= Time.time - delay + Mathf.Epsilon)
+        {
+            transform.position = Vector3.Lerp(transform.position, pointsInSpace.Dequeue().Position + offset, Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Castle Shock/Assets/C# Code/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float reloadTime = 0.4f;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && reloadTime <= 0)
+        {
+            Shoot();
+            reloadTime = 0.25f;
+        }
+        reloadTime -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+    }
+}
